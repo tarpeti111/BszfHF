720ae340031bfd1f26a4a2b3b3d202eb084a164c
Tar Péter <tarpeti111@gmail.com>
2025-10-15 12:11:45 +0200

    Added new timer for game ticks, made timescaleing, made code more readable
    It should be broken right now. WIP

--- main.c
+++ main.c
@@ -15,25 +15,30 @@
 #define V 0x2230
 #define R 0x0c73
 
+volatile uint16_t gameTicks;
 volatile uint8_t animationFrame;
 
 // TIMER0 interrupt handler
 void TIMER0_IRQHandler(void) {
   TIMER_IntClear(TIMER0, TIMER_IF_OF); // Clear overflow interrupt flag
-  animationFrame++;
-  animationFrame %= 11;
+  //animationFrame++;
+  //animationFrame %= 11;
+}
+void TIMER1_IRQHandler(void) {
+  TIMER_IntClear(TIMER1, TIMER_IF_OF); // Clear overflow interrupt flag
+    animationFrame++;
+    animationFrame %= 11;
 }
 
 // Scroll Animation
-void animate(SegmentLCD_LowerCharSegments_TypeDef lowerCharSegments, char *frames){
-  lowerCharSegments[0].raw = frames[(animationFrame + 0) % 11];
-  lowerCharSegments[1].raw = frames[(animationFrame + 1) % 11];
-  lowerCharSegments[2].raw = frames[(animationFrame + 2) % 11];
-  lowerCharSegments[3].raw = frames[(animationFrame + 3) % 11];
-  lowerCharSegments[4].raw = frames[(animationFrame + 4) % 11];
-  lowerCharSegments[5].raw = frames[(animationFrame + 5) % 11];
-  lowerCharSegments[6].raw = frames[(animationFrame + 6) % 11];
-  // only increment asd every 0.5 seconds or so, dont use a delay, use a peripheral timer
+void animate(SegmentLCD_LowerCharSegments_TypeDef *lowerCharSegments, uint16_t *frames, uint8_t maxFrames){
+  lowerCharSegments[0].raw = frames[(animationFrame + 0) % maxFrames];
+  lowerCharSegments[1].raw = frames[(animationFrame + 1) % maxFrames];
+  lowerCharSegments[2].raw = frames[(animationFrame + 2) % maxFrames];
+  lowerCharSegments[3].raw = frames[(animationFrame + 3) % maxFrames];
+  lowerCharSegments[4].raw = frames[(animationFrame + 4) % maxFrames];
+  lowerCharSegments[5].raw = frames[(animationFrame + 5) % maxFrames];
+  lowerCharSegments[6].raw = frames[(animationFrame + 6) % maxFrames];
 }
 
 void USART_TxString(USART_TypeDef *usart, char *data, uint8_t lenght){
@@ -44,9 +49,15 @@ void USART_TxString(USART_TypeDef *usart, char *data, uint8_t lenght){
 
 int main(void)
 {
+  uint16_t gameOverFrames[] = {G,A,M,E,0,O,V,E,R,0,0};
+  uint8_t cursorPosition = 0;
+  bool b_gameover = false;
+  volatile uint8_t timeScale = 10;
+
   CMU_ClockEnable(cmuClock_GPIO,true);
   CMU_ClockEnable(cmuClock_UART0,true);
   CMU_ClockEnable(cmuClock_TIMER0, true);
+  CMU_ClockEnable(cmuClock_TIMER1, true);
 
   USART_InitAsync_TypeDef init = USART_INITASYNC_DEFAULT;
   USART_InitAsync(UART0, &init);
@@ -59,18 +70,27 @@ int main(void)
   SegmentLCD_Init(false);
   SegmentLCD_LowerCharSegments_TypeDef lowerCharSegments[SEGMENT_LCD_NUM_OF_LOWER_CHARS];
 
+  // GAME TICK TIMER
   // ---------------- TIMER0 SETUP ----------------
-  TIMER_Init_TypeDef timerInit = TIMER_INIT_DEFAULT;
-  timerInit.prescale = timerPrescale1024;
-  TIMER_Init(TIMER0, &timerInit);
+  TIMER_Init_TypeDef timer0Init = TIMER_INIT_DEFAULT;
+  timer0Init.prescale = timerPrescale1024;
+  TIMER_Init(TIMER0, &timer0Init);
   TIMER_TopSet(TIMER0, 6836);
   TIMER_IntEnable(TIMER0, TIMER_IF_OF);
   NVIC_EnableIRQ(TIMER0_IRQn);
-  TIMER_Enable(TIMER0, true);
+  //TIMER_Enable(TIMER0, true);
   // ------------------------------------------------
-  uint8_t cursorPosition = 0;
-  bool b_gameover = false;
 
+  // ANIMATION TIMER
+  // ---------------- TIMER1 SETUP ----------------
+  TIMER_Init_TypeDef timer1Init = TIMER_INIT_DEFAULT;
+  timer1Init.prescale = timerPrescale1024;
+  TIMER_Init(TIMER1, &timer1Init);
+  TIMER_TopSet(TIMER1, 13672);
+  TIMER_IntEnable(TIMER1, TIMER_IF_OF);
+  NVIC_EnableIRQ(TIMER1_IRQn);
+  TIMER_Enable(TIMER1, true);
+  // ------------------------------------------------
   while(1){
       // CLEAR LOWER SEGMENTS
       for (uint8_t p = 0; p < SEGMENT_LCD_NUM_OF_LOWER_CHARS; p++) {
@@ -84,34 +104,49 @@ int main(void)
         char recieved = USART_Rx(UART0);
         //debug input -> output
         USART_Tx(UART0,recieved);
+        uint8_t lastTimeScale = timeScale;
+        switch (recieved){
+          case 'd':
+            if(!b_gameover)
+              cursorPosition++;
+            break;
+          case 'a':
+            if(!b_gameover)
+              cursorPosition++;
+            break;
+          case '+':
+            timeScale++;
+            break;
+          case '-':
+            timeScale--;
+            break;
+          case 'r':
+            b_gameover = !b_gameover;
+            for(int i  = 0; i < 50; i++){
+                USART_Tx(UART0, '\n');
+            }
+            USART_Tx(UART0, '\r');
+            break;
+          default:
+            break;
+        }
+        cursorPosition %= 6;
+        if(timeScale != lastTimeScale){
+            if(timeScale < 1){
+                timeScale = 1;
+            }
+            TIMER_TopSet(TIMER1, 1367*timeScale);
 
-        if(!b_gameover){
-            if(recieved == 'd' && cursorPosition < 6){
-                cursorPosition++;
-            }
-            else if(recieved == 'a' && cursorPosition > 0){
-                cursorPosition--;
-            }
-            else if(recieved == '+'){
-                //speed up
-            }
-            else if(recieved == '-'){
-                //slow down
+            if(TIMER_CounterGet(TIMER1) >= 1367*timeScale){
+              TIMER_CounterSet(TIMER0, 0);
             }
         }
-      if(recieved == 'r'){
-          b_gameover = !b_gameover;
-          for(int i  = 0; i < 50; i++){
-              USART_Tx(UART0, '\n');
-          }
-          USART_Tx(UART0, '\r');
-      }
     }
     if(!b_gameover){
         lowerCharSegments[cursorPosition].d = 1;
     }
     if(b_gameover){
-
+        animate(lowerCharSegments, gameOverFrames, 11);
     }
     //SegmentLCD_Number(cursorPosition);
     SegmentLCD_LowerSegments(lowerCharSegments);
